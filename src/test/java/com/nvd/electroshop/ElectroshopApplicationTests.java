2969bd32cad347108cb0f88ec233bdea875b8138
Ngo Vu Duy <ngovuduypc@gmail.com>
2025-07-15 18:25:51 +0700

    init eshop project

---
+++ src/test/java/com/nvd/electroshop/ElectroshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvd.electroshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
